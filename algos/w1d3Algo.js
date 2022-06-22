55dec812fefb40fc563125463a1152c675bce1fb
spedi123 <peteran8604@gmail.com>
2022-06-22 09:28:46 -0700

    add icons

---
+++ algos/w1d3Algo.js
@@ -0,0 +1,18 @@
+//data types
+/*
+Datat types can include the following:
+1. Numbers (integers and decimals(floats)) -> 1, 2, 3, 99
+2. Strings -> Characters and leters combined into one using quotes 
+3. Boleans -> ture or false
+*/
+
+// camelCase
+// snake_case
+// PascalCase
+
+let name = "Peter An";
+let age = 29;
+let gasPrice = 5.29;
+let woke = true;
+
+console.log("good" + name);
