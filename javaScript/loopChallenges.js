82362eb7d3ca2cb992a32399da1824642ec1015a
spedi123 <peteran8604@gmail.com>
2022-06-25 17:33:57 -0700

    finished loop challenges

---
+++ javaScript/loopChallenges.js
@@ -0,0 +1,37 @@
+// 1. Print odds 1~20
+
+// for (var i = 0; i <= 20; i++) {
+//   if (i % 2 != 0) {
+//     console.log(i);
+//   }
+// }
+
+// 2. Decreasing Multiples of 3
+
+// for (var i = 100; i > 0; i--) {
+//   if (i % 3 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// 3. Print the sequence
+
+// for (var i = 4; i > -4; i -= 1.5) {
+//   console.log(i);
+// }
+
+// 4. Sigma
+
+// var sum = 0;
+// for (var i = 1; i <= 100; i++) {
+//   sum += i;
+//   console.log(sum);
+// }
+
+// 5. Factorial
+
+// var multiples = 1;
+// for (var i = 1; i < 13; i++) {
+//   multiples *= i;
+// }
+// console.log(multiples);
